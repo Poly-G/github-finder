a8a32452655962415374edf9fe837422aaebd7b8
Poly Goveia <poly.goveia@gmail.com>
2019-06-06 14:04:18 -0700

    add project CSS

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
 };
 
 export default App;
