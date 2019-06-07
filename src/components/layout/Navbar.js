f970cc151261a48f50aeb6a9310f7c5d309aad4b
Poly Goveia <poly.goveia@gmail.com>
2019-06-06 17:34:18 -0700

    convert NavBar into a functional componenet

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className="fab fa-github" /> GitHub Finder
-        </h1>
-      </nav>
-    );
-  }
-}
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className="fab fa-github" /> GitHub Finder
+      </h1>
+    </nav>
+  );
+};
 
 export default Navbar;
