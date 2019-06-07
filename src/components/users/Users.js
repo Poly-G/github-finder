738869731d3ae91d42c019799c07e9bacd00534b
Poly Goveia <poly.goveia@gmail.com>
2019-06-06 19:25:22 -0700

    refactor Users.js into a functional component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
 // Components
 import UserItem from "./UserItem";
 
-class Users extends Component {
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.props.users.map(user => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    );
-  }
-}
+const Users = props => {
+  return (
+    <div style={userStyle}>
+      {props.users.map(user => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
 
 const userStyle = {
   display: "grid",
