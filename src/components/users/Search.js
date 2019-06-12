8a5aae5910d36e52acba46bba36b214f9c63ceea
Poly Goveia <poly.goveia@gmail.com>
2019-06-11 18:02:23 -0700

    add state to Search bar

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  state = {
+    text: ""
+  };
+
+  onChange = e => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+
   render() {
     return (
       <div>
         <form className="form" />
-        <input type="text" name="text" placeholder="Search users..." />
+        <input
+          type="text"
+          name="text"
+          placeholder="Search users..."
+          value={this.state.text}
+          onChange={this.onChange}
+        />
         <input
           type="submit"
           value="Search"
