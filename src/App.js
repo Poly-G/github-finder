ae4584e2001bab464ffe329b31e5cb7a3b2a449c
Poly Goveia <poly.goveia@gmail.com>
2019-06-14 19:21:10 -0700

    add User componenet

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
+import User from "./components/users/User";
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -31,6 +33,22 @@ class App extends Component {
 
     this.setState({
       users: res.data.items,
+      loading: false
+    });
+  };
+
+  // Get a single GitHub user
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `http://api.github.com/users/${username}?&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({
+      user: res.data,
       loading: false
     });
   };
@@ -59,7 +77,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
       <Router>
@@ -84,6 +102,18 @@ class App extends Component {
                 )}
               />
               <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
