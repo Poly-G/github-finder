521de90d5a7b0120643ece231eb809cb9d0fcc18
Poly Goveia <poly.goveia@gmail.com>
2019-06-14 18:21:18 -0700

    add links to about and home

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import UserItem from "../users/UserItem";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -8,6 +9,10 @@ const Navbar = () => {
       <h1>
         <i className="fab fa-github" /> GitHub Finder
       </h1>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </ul>
     </nav>
   );
 };
